607cfaf918db9be38e88b99a873be1645f93b3ae
junseokkim <jsutin639@gmail.com>
2021-10-11 19:57:59 +0900

    Add Lecture6

---
+++ Lecture6/index.js
@@ -0,0 +1,28 @@
+let firstName = "Tammer";
+let lastName = " Mike";
+let age = 30;
+let weight = 75.5;
+let graduated = true;
+
+age = null;
+console.log("Type of Null :",typeof age);
+
+
+console.log(typeof age);
+console.log(typeof weight);
+
+age = "old"
+console.log("Age later :",typeof age);
+
+console.log("What you can do with string types.");
+console.log("Length" ,firstName.length);
+console.log(firstName.indexOf("y"))
+
+age = parseInt("52 this is fun.");
+console.log(age);
+age = parseInt("This is fun 42.");
+console.log(age);
+
+
+console.log("Hello");
+console.log("15+2-7");
