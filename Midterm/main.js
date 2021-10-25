aab2e4c2e094bc902717b5476d8248bc93258b8e
justin639 <justin639@gmail.com>
2021-10-25 18:31:20 +0900

    Add routing to welcomePage.html

--- Midterm/main.js
+++ Midterm/main.js
@@ -1,6 +1,24 @@
 let inputStr = document.getElementById('inputNum');
-let accountNum = [1111111111111111, 1111111111111110];
+let accountData = [{num: '1111111111111111', pin: '0000'}, {num: '1111111111111110', pin: '0001'}];
 let statePIN = false;
+let currentUID = -1; // uid -1 means no UID available
+let count = 5;
+
+function changeState(uid) {
+    if (statePIN) {
+        document.getElementById('initTitle').hidden = true;
+        document.getElementById('inputInst').innerText = 'Please enter your PIN:'
+        inputStr.placeholder = 'Enter PIN';
+        currentUID = uid;
+        console.log("Account auth successful");
+    } else {
+        document.getElementById('initTitle').hidden = false;
+        document.getElementById('inputInst').innerText = 'Enter your 16 digit account number or insert your card below:'
+        inputStr.placeholder = 'Enter Account Number';
+        currentUID = uid;
+        console.log("Go back to main page.");
+    }
+}
 
 function clickNum(number) {
     inputStr.value += number;
@@ -14,29 +32,43 @@ function deleteNum() {
 
 function authAccount() {
     if (inputStr.value.length < 16) {
-        alert("Account Number is less than 16-digit.");
-        inputStr.value = "";
+        alert("Account Number is less than 16 - digit.");
     } else if (inputStr.value.length > 16) {
-        alert("Account Number is more than 16-digit.");
-        inputStr.value = "";
+        alert("Account Number is more than 16 - digit.");
     } else {
-        let account = parseInt(inputStr.value);
-        for (let i = 0; i < accountNum.length; i++) {
-            console.log(accountNum[i] + ' : '+ account);
-            if (accountNum[i] === account) {
-                //enter PIN submit
-                statePIN =true;
-                document.getElementById('initTitle').hidden = true;
-                document.getElementById('inputInst').innerText='Please enter your PIN:'
-                inputStr.placeholder = 'Enter PIN';
-                account=i;
-                console.log("Account auth successful");
+        let account = inputStr.value;
+        // find matching user number
+        for (let i = 0; i < accountData.length; i++) {
+            console.log(accountData[i].num + ' : ' + account);
+            if (accountData[i].num === account) {
+                statePIN = true;
+                changeState(i);
+                account = i;
                 break;
             }
         }
-        if(account === inputStr.value.length){
+        if (account === inputStr.value.length) {
             alert("That account number does not exist!");
         }
-        inputStr.value = "";
     }
+    inputStr.value = "";
+}
+
+function authPIN() {
+    if(inputStr.value === accountData[currentUID].pin){
+        // open welcome page
+        window.location.href = 'welcomePage.html'
+    }
+    else{
+        count--;
+        if (count === 0) {
+            alert("You don't have any attempts left. Going back to main page.");
+            statePIN = false;
+            count = 5;
+            changeState(-1)
+        } else {
+            alert("Incorrect PIN. You have " + count + " attempts left.");
+        }
+    }
+    inputStr.value = "";
 }
